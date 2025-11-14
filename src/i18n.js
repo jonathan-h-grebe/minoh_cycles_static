16853edc85ad425ea968e84d335740ab09aec087
jona <jonathanhill381@gmail.com>
2025-11-14 17:40:52 +0900

    try to auto detect language

--- src/i18n.js
+++ src/i18n.js
@@ -2,9 +2,18 @@ import { createI18n } from 'vue-i18n'
 import en from './locales/en.json'
 import ja from './locales/ja.json'
 
+// Detect browser language
+function getBrowserLocale() {
+  const browserLanguage = navigator.language || navigator.userLanguage
+  const languageCode = browserLanguage.split('-')[0] // Get 'ja' from 'ja-JP' or 'en' from 'en-US'
+
+  // Return 'ja' if Japanese is detected, otherwise default to 'en'
+  return languageCode === 'ja' ? 'ja' : 'en'
+}
+
 export default createI18n({
   legacy: false, // Use Composition API mode
-  locale: 'en', // default locale
+  locale: getBrowserLocale(), // Set locale based on browser language
   fallbackLocale: 'en',
   messages: {
     en,
