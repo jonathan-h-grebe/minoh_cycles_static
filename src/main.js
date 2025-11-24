12bc2b37f27535de8024e8585d7574810d22f704
jona <jonathanhill381@gmail.com>
2025-11-24 14:02:29 +0900

    added fonts to package to improve performance

--- src/main.js
+++ src/main.js
@@ -4,4 +4,12 @@ import router from './router'
 import i18n from './i18n.js'
 import './assets/main.css'
 
+// Import fonts
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/700.css'
+import '@fontsource/montserrat/800.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/800.css'
+
 createApp(App).use(router).use(i18n).mount('#app')
